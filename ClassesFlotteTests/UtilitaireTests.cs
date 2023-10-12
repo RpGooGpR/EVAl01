504ca61ce50ee1e96eb217d60f81762c03442824
Mateo <rametmateo@gmail.com>
2023-10-12 11:53:22 +0200

    Eval01

---
+++ ClassesFlotteTests/UtilitaireTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassesFlotte;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesFlotte.Tests
+{
+    [TestClass()]
+    public class UtilitaireTests
+    {
+        [TestMethod()]
+        public void ChargeUtileTest()
+        {
+            Utilitaire  utilitaire = new Utilitaire(500, 300 ,15, "AA-125-BB", "Essence", 130 );
+            
+        }
+
+        [TestMethod()]
+        public void PrevoirEntretienTest()
+        {
+            // TODO
+        }
+    }
+}
